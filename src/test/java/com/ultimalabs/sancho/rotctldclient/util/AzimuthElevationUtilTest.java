ce12de760e6cd439e54d087e16cfe4ba626518b7
Darko Topolko <darko@ultima.hr>
2019-12-11 09:57:19 +0100

    Updated configuration for host without the rotator

---
+++ src/test/java/com/ultimalabs/sancho/rotctldclient/util/AzimuthElevationUtilTest.java
@@ -0,0 +1,27 @@
+package com.ultimalabs.sancho.rotctldclient.util;
+
+import com.ultimalabs.sancho.rotctldclient.model.AzimuthElevation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AzimuthElevationUtilTest {
+
+    @DisplayName("Test normalizeAngle()")
+    @Test
+    void normalizeAngle() {
+
+        assertEquals(180, AzimuthElevationUtil.normalizeAngle(180));
+        assertEquals(44, AzimuthElevationUtil.normalizeAngle(404));
+        assertEquals(180, AzimuthElevationUtil.normalizeAngle(-180));
+        assertEquals(0, AzimuthElevationUtil.normalizeAngle(360));
+
+        assertEquals(0, AzimuthElevationUtil.normalizeAngle(0.4));
+        assertEquals(21, AzimuthElevationUtil.normalizeAngle(20.51));
+        assertEquals(340, AzimuthElevationUtil.normalizeAngle(-19.7));
+        assertEquals(180, AzimuthElevationUtil.normalizeAngle(-540.0));
+
+    }
+
+}
